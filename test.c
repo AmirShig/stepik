279dbb2ff6236d757894fe591551dbb8713fa5dc
AmirShig <shigapov_amir@mail.ru>
2023-01-22 23:28:37 +0300

    first commit

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	char t = 'G';
+	printf("char = %d\n", t);
+	return 0;
+}
---
+++ test2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() 
+{
+	double x = 9.123456789;
+	printf("%.9f\n", x);
+	printf("%.9lf\n", x);
+	return 0;
+}
